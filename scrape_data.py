33ec74d6ad6df4af905e74f48b4a3b0ac5df12f0
Maximilian Zachow <maxzachow@protonmail.com>
2022-11-04 16:31:24 +0100

    add gitignore file

--- scrape_data.py
+++ scrape_data.py
@@ -1,1 +1,16 @@
-"""Scrape data""" 
+#!/usr/bin/env python
+import apccapi
+
+c = apccapi.Client()
+c.retrieve(
+	{
+		'jobtype': 'MODEL',
+		'dataset': 'MODEL',
+		'type': 'HINDCAST',
+		'institute': 'BCC',
+		'model': 'CSM1.1M',
+		'variable': ['prec', 't2m'],
+		'yearmonth': ['201909']
+	},
+	'bcc.zip'
+)
